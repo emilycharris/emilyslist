c722a0a8905fcb9195a6226fe4d05226942982a5
emilycharris <emily.harris.mail@gmail.com>
2016-06-23 16:13:52 -0400

    working on models

--- classifieds/admin.py
+++ classifieds/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from classifieds.models import Region, Listing, Profile
+
 
 # Register your models here.
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ["title", "price", "location", "body", "photo"]
+    search_fields = ["body"]
+
+admin.site.register(Listing, ListingAdmin)
+
+admin.site.register(Region)
+
+admin.site.register(Profile)
--- classifieds/models.py
+++ classifieds/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Region(models.Model):
+    location = models.CharField(max_length=50)
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=50)
+    price = models.DecimalField(decimal_places=2, max_digits=15)
+    location = models.ForeignKey(Region)
+    created = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+    photo = models.ImageField(upload_to="profile_photos", null=True, blank=True)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField("auth.User")
+    location = models.ForeignKey(Region)
